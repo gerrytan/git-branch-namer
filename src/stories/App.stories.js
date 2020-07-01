8b224a22fc2336a89415b29f443d4c91b1042c71
Gerry Tan <gerry.g.tan@gmail.com>
2020-07-02 00:23:36 +1000

    Window size

--- src/stories/App.stories.js
+++ src/stories/App.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "../App";
 import "../index.css";
+import "./App.stories.css";
 
 export default {
   title: "<App>",
@@ -8,4 +9,8 @@ export default {
 
 const useWindowTitleMock = () => ["foobar"];
 
-export const main = () => <App useWindowTitle={useWindowTitleMock} />;
+export const main = () => (
+  <div class="container">
+    <App useWindowTitle={useWindowTitleMock} />
+  </div>
+);
