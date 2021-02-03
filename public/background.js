8fa2eb44306839f591b47399cf508eac983d083c
Gerry Tan <gerry.g.tan@gmail.com>
2021-02-04 01:05:26 +1100

    WIP: working on a background.js file
    which hopefully can run in the page and communicate with popup

---
+++ public/background.js
@@ -0,0 +1,5 @@
+/*
+Warning: this file is not built by webpack due to difficulty in re-configuring a second
+js-only entry in create-react-app even after ejecting.
+*/
+console.log("hi im a background js");
